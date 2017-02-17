8720b886f52272455c47faee36637a4162868c63
Johnny <po_bo@hotmail.com>
2017-02-16 21:14:33 -0800

    created react env, set up index and home router path, added s3plugin and dotenv to webpack config. created home, main and nav component

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+var webpack = require('webpack');
+var S3Plugin = require('webpack-s3-plugin');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'client/public');
+var APP_DIR = path.resolve(__dirname, 'client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new S3Plugin({
+      s3Options: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: 'us-west-1'
+      },
+      s3UploadOptions: {
+        Bucket: 'snapflexapp.com'
+      }
+    })
+  ]
+};
+
+module.exports = config;
+
