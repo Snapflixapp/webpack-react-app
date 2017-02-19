72efcb4abd87ee78e856e5a1de32326728a47e34
michaelmerrill <michael@michaelmerrill.io>
2017-02-18 17:59:49 -0800

    Implement navbar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from './Main.css'
 
 const Main = (props) => (
-  <div className={styles.main} {...props} />
+  <div className={styles.main}>
+    <div className={styles.container} {...props} />
+  </div>
 )
 
 export default Main
