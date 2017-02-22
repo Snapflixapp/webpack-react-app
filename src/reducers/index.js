c52d5c8837c5c4139513dc75f4c047c4b17d3157
Johnny <po_bo@hotmail.com>
2017-02-22 15:26:19 -0800

    added comment and re push

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
+// combine all reducers
 import {combineReducers} from 'redux'
 import userReducer from './UserReducer'
 import usersReducer from './UsersReducer'
 import videosReducer from './VideosReducer'
 import usersDataReducer from './UsersDataReducer'
+
 
 // combine all reducers
 const allReducers = combineReducers({
