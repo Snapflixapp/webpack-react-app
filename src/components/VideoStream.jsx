c8dd9bd64794b1fbbfcd75a738fbf88aea65133e
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-02-25 15:55:02 -0800

    (feat) Cleanup Video component

--- src/components/VideoStream.jsx
+++ src/components/VideoStream.jsx
@@ -1,36 +1,20 @@
 import React, {Component} from 'react'
 import styles from './VideoStream.css'
+// import RecordPage from './s3UploadPage'
 
 class VideoStream extends Component {
   constructor (props) {
     super(props)
 
-    this.state = {
-      videoCapture: {}
-    }
-
-    this.handlePlay = this.handlePlay.bind(this)
-  }
-
-  handlePlay () {
-    console.log('handle play')
-    this.refs.video.play()
   }
 
   render () {
     return (
       <div className={styles.main}>
-        <video ref='video' autoPlay='true' src={this.props.src} width={this.props.width} height={this.props.height} />
-        <button onClick={this.handlePlay}>Play</button>
+        <video src={this.props.src} width={this.props.width} height={this.props.height} autoPlay/>
       </div>
     )
   }
 }
-
-// const VideoStream = (props) => (
-//   <div className={styles.main}>
-//     <video autoPlay='true' src={props.src} width={props.width} height={props.height} />
-//   </div>
-// )
 
 export default VideoStream
