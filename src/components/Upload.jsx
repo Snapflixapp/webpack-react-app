5a8a8f3907378781f94e697cb871b2d02671058f
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-02-25 15:54:04 -0800

    (feat) add upload path to database

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 import styles from './Upload.css'
-
+import { captureUserMedia, S3Upload } from './Utils';
 import VideoStream from './VideoStream'
-import Canvas from './Canvas'
+import RecordRTC from 'recordrtc'
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
-navigator.msGetUserMedia || navigator.oGetUserMedia
+// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+// navigator.msGetUserMedia || navigator.oGetUserMedia
+
+const hasGetUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
 
 export class Upload extends Component {
   constructor (props) {
@@ -14,38 +18,75 @@ export class Upload extends Component {
     this.state = {
       src: null,
       width: 300,
-      height: 300
+      height: 300,
+      uploadSuccess: null,
+      uploading: false,
+      recordVideo: null
     }
 
-    this.handleVideo = this.handleVideo.bind(this)
-    this.videoError = this.videoError.bind(this)
+    this.requestUserMedia = this.requestUserMedia.bind(this);
+    this.startRecord = this.startRecord.bind(this);
+    this.stopRecord = this.stopRecord.bind(this);
   }
 
-  handleVideo (stream) {
-    console.log('handlevideo function')
-    this.setState({
-      src: window.URL.createObjectURL(stream)
-    })
-  }
-
-  videoError () {
-    console.log('videoerror function')
-  }
-
-  componentDidMount () {
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({
-        video: true
-      }, this.handleVideo, this.videoError)
+  componentDidMount() {
+    if(!hasGetUserMedia) {
+      alert("Your browser cannot stream from your webcam. Please switch to Chrome or Firefox.");
+      return;
     }
+    this.requestUserMedia();
+  }
+
+  requestUserMedia() {
+    console.log('requestUserMedia')
+    captureUserMedia((stream) => {
+      this.setState({ src: window.URL.createObjectURL(stream) });
+      console.log('setting state', this.state)
+    });
+  }
+
+  startRecord() {
+    captureUserMedia((stream) => {
+      this.state.recordVideo = RecordRTC(stream, { type: 'video' });
+      this.state.recordVideo.startRecording();
+    });
+
+    setTimeout(() => {
+      this.stopRecord();
+    }, 4000);
+  }
+
+  stopRecord() {
+    this.state.recordVideo.stopRecording(() => {
+      let params = {
+        type: 'video/webm',
+        data: this.state.recordVideo.blob,
+        id: Math.floor(Math.random()*90000) + 10000
+      }
+      console.log(this.state)
+      this.setState({ uploading: true });
+
+      S3Upload(params)
+      .then((success) => {
+        console.log('enter then statement')
+        if(success) {
+          console.log(success)
+          this.setState({ uploadSuccess: true, uploading: false });
+        }
+      }, (error) => {
+        alert(error, 'error occurred. check your aws settings and try again.')
+      })
+    });
   }
 
   render () {
     return (
       <div className={styles.main}>
         Upload Page
-        <VideoStream src={this.state.src} width={this.state.width} height={this.state.height} />
-        <Canvas width={this.state.width} height={this.state.height} />
+        <VideoStream src={this.state.src} ref='video' width={this.state.width} height={this.state.height} />
+       {this.state.uploading ?
+          <div>Uploading...</div> : null}
+        <div><button onClick={this.startRecord}>Start Record</button></div>
       </div>
     )
   }
