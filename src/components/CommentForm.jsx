08bb4366f4b02c65ddd2bf398e9516ae399f7a8a
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-03-09 20:15:25 -0800

    implemented comment form and added css file

---
+++ src/components/CommentForm.jsx
@@ -0,0 +1,81 @@
+import React, {Component} from 'react'
+import styles from './Comment.css'
+
+class CommentForm extends Component {
+  constructor (props) {
+    super(props)
+    
+    this.state = {
+      comments: [],
+      author: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit (e) {
+    e.preventDefault()
+    const oldComments = this.state.comments
+    this.setState({
+      comments: [...oldComments, this.refs.comment.value],
+      author: this.refs.author.value
+    })
+    this.refs.CommentForm.reset()
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.comments.map((comment, index) => (
+          <div key={index} >
+            <p><strong>{`${this.state.author}: `}</strong> {comment}</p>
+          </div>
+        ))}
+        <form ref='CommentForm'>
+          <input className={styles.inputs} type='text' ref='author' placeholder='author' />
+          <input className={styles.inputs} type='text' ref='comment' placeholder='Comment' />
+          <button type='submit' onClick={this.handleSubmit} >Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+// class CommentForm extends Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = {
+// 			comments: ['first comment!', 'jasmine is awesome', 'this is cool!'],
+//       localComments: []
+//       current:comment
+// 		}
+//     this.handleLocalComments = this.handleLocalComments.bind(this)
+// 	}
+  
+//   handleLocalComments (e) {
+//     console.log(e.target)
+//     e.preventDefault()
+//     let currentComments = this.state.localComments
+//     this.setState({
+//       localComments: [...currentComments, e.target.value]
+//     })
+//     console.log(this.state.localComments)
+//     // this.form.ref.reset()
+//   }
+//   render () {
+//     return (
+//         <div>
+//           <div>
+//             List of Comments:
+//               {this.state.localComments.map((comment, index) => {
+//                 return <p key={index} >{comment}</p>
+//               })}
+//           </div>
+//           <div>
+//               <input type="text" ref="comment" placeholder="write a comment..."></input>
+//               <button type="submit" onClick={this.handleLocalComments}>post</button>
+//           </div>
+//         </div>
+//       )
+//   }
+// }
+
+export default CommentForm
